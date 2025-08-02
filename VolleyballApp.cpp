c9eb0788b608192f7e5edcd03ba2e7b2612c00ce
RicardoBrando <117677323+RicardoBrando@users.noreply.github.com>
2025-08-02 14:12:56 +0200

    Add project files.

---
+++ VolleyballApp.cpp
@@ -0,0 +1,12 @@
+﻿// VolleyballApp.cpp : Defines the entry point for the application.
+//
+
+#include "VolleyballApp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
